a20125770c8760421fca91bd21347fa5fb835791
Alex Grönholm <alex.gronholm@nextday.fi>
2022-05-03 20:49:31 +0300

    Don't inherit docstrings
    This gets rid of the useless start() method docs.

--- docs/conf.py
+++ docs/conf.py
@@ -29,6 +29,7 @@ exclude_patterns = ["_build"]
 pygments_style = "sphinx"
 highlight_language = "python3"
 todo_include_todos = False
+autodoc_inherit_docstrings = False
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
